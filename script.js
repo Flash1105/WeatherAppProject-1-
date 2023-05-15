a614a5f7aede44f699dffadd7e5a44bbe7d3e5be
Flash1105 <126614546+Flash1105@users.noreply.github.com>
2023-05-14 23:37:10 -0400

    Update script.js
    clean up

--- script.js
+++ script.js
@@ -14,8 +14,6 @@ weatherForm.addEventListener('submit', function (event) {
   fetchWeatherData(location);
 });
 
-
-
 function fetchWeatherData(location) {
   const apiKey = 'fbbde147c33691fe80920cb909533f63';  //this is the API key you will need to change.
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`;
